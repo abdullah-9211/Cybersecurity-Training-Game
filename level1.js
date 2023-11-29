fe6f54047b31aba2ef0a2e8b3cf31c4034b22845
aleena30-05 <43938991+aleena30-05@users.noreply.github.com>
2023-11-30 00:19:35 +0500

    FIX: start Screen UI

--- level1.js
+++ level1.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $("#cipher-label").text("Decode the phrase: Gur Pnpur vf Fvpxre");
+  $("#cipher-label").text("Decode the phrase: hmnphj symxyn");
 
   // Add event listener for the submit button
   $("#submit-button").click(function() {
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var userInput = $("#cipher-input").val().toLowerCase();
 
     // Check if the input matches the correct answer
-    if (userInput === "the chicken is chicken") {
+    if (userInput === "chicken drumsticks") {
       // Redirect to the next level or perform next level logic
       window.location.href = "level2.html";
     } else {
