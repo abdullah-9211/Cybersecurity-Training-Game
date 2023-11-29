4b3d751d6522f0ce535afeda4c9eb4de9580415f
aleena30-05 <43938991+aleena30-05@users.noreply.github.com>
2023-11-29 19:38:32 +0500

    ADD: frontend of start two screens

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // jQuery code can go here for additional interactivity
+  
+    // Example: Click event for the start button
+    $("#start-button").click(function() {
+      alert("Game is starting!");
+      // Add more game initialization logic here
+    });
+  });
+  
