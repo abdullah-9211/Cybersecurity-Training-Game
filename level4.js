153c453f53e252f3ce077688220aa5422a0b4ef1
aleena30-05 <43938991+aleena30-05@users.noreply.github.com>
2023-11-30 16:31:11 +0500

    ADD: all levels, change texts later

--- level4.js
+++ level4.js
@@ -1,46 +1,49 @@
 $(document).ready(function() {
-    generateChallenge();
-  });
-  
-  // Function to generate the single challenge with multiple options dynamically
-  function generateChallenge() {
-    var challengeContainer = $("#challenge-container");
-  
-    var challenge = {
+  generateChallenge();
+});
+
+// Declare challenge as a global variable
+var challenge;
+
+// Function to generate the single challenge with multiple options dynamically
+function generateChallenge() {
+  var challengeContainer = $("#challenge-container");
+
+  // Assign the challenge globally
+  challenge = {
       question: "Which image represents a phishing email?",
       options: [
-        "./assets/correct_email_example.png",
-        "./assets/phishing_example.png",
-        "./assets/correct_email_example_2.png"
+          "./assets/correct_email_example.png",
+          "./assets/phishing_example.png",
+          "./assets/correct_email_example_2.png"
       ],
       correctOption: 1
-    };
-  
-    var challengeElement = `<div class="challenge">`;
-    challengeElement += `<p>${challenge.question}</p>`;
-  
-    challenge.options.forEach(function(option, optionIndex) {
+  };
+
+  var challengeElement = `<div class="challenge">`;
+  challengeElement += `<p>${challenge.question}</p>`;
+
+  challenge.options.forEach(function(option, optionIndex) {
       challengeElement += `<img src="${option}" class="challenge-option" data-option="${optionIndex}" alt="Option ${optionIndex + 1}">`;
-    });
-  
-    challengeElement += "</div>";
-  
-    challengeContainer.append(challengeElement);
-  
-    $(".challenge-option").click(function() {
+  });
+
+  challengeElement += "</div>";
+
+  challengeContainer.append(challengeElement);
+
+  $(".challenge-option").click(function() {
       var selectedOption = $(this).data("option");
-  
+
       handleSingleChallenge(selectedOption);
-    });
-  }
-  
-  // Function to handle the single challenge with multiple options
-  function handleSingleChallenge(selectedOption) {
-    if (selectedOption === challenge.correctOption) {
+  });
+}
+
+// Function to handle the single challenge with multiple options
+function handleSingleChallenge(selectedOption) {
+  if (selectedOption === challenge.correctOption) {
       alert("Correct! You've successfully navigated the Labyrinth of Challenges.");
       window.location.href = "level5.html";
-    } else {
+  } else {
       alert("Incorrect! Try again.");
-    }
   }
-  
+}
---
+++ level5.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    // Simulated credentials (you would dynamically load or generate this)
+    var correctUsername = "cluckles";
+    var correctPassword = "eggs123";
+  
+    // Add event listener for the authenticate button
+    $("#authenticate-button").click(function() {
+      // Get the input values
+      var enteredUsername = $("#username-input").val().trim();
+      var enteredPassword = $("#password-input").val().trim();
+  
+      // Check if the entered credentials are correct
+      if (enteredUsername === correctUsername && enteredPassword === correctPassword) {
+        // Display success message
+        alert("Two-Factor Authentication successful! You may now proceed.");
+        // Enable the proceed button
+        $("#proceed-button").removeAttr("disabled");
+      } else {
+        // Display an error message
+        alert("Incorrect credentials! Try again.");
+      }
+    });
+  
+    // Add event listener for the proceed button
+    $("#proceed-button").click(function() {
+      // Redirect to the next level or perform next level logic
+      alert("Proceeding to the next challenge.");
+      window.location.href = "end.html";
+    });
+  });
+  
